e3c95aad91c42e71bd29f66ef44e5686c0b52251
lumiaczek <danielborowski05@gmail.com>
2022-08-16 14:18:14 +0200

    Initial commit, installed Tailwindcss, Tauri and SvelteKit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<section
+  class="flex min-h-screen bg-white justify-between items-center flex-col p-5 w-full"
+>
+  <h1 class="text-zhpg-light text-3xl text-center font-sans m-2">
+    Witaj w <strong>RajdApp!</strong>
+  </h1>
+  <img src="./gory.png" alt="Obrazek gór" />
+  <div class="space-y-2 w-full">
+    <button
+      class="rounded-lg shadow border-2 bg-zhpg-light text-white p-2 w-full font-bold font-sans"
+      >Zaloguj</button
+    >
+    <button
+      class="rounded-lg shadow border-2 bg-zhpg-light text-white p-2 w-full font-bold font-sans"
+      >Załóż konto</button
+    >
+  </div>
+</section>
